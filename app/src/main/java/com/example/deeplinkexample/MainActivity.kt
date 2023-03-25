72ca95a53778fdc16939e2162c6c7a4f4ed46a5f
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-03-26 00:57:46 +0330

    feat: add canvas

--- app/src/main/java/com/example/deeplinkexample/MainActivity.kt
+++ app/src/main/java/com/example/deeplinkexample/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.deeplinkexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -18,7 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.text.font.FontFamily
@@ -28,7 +33,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.deep.deepSdk
 import com.example.deeplinkexample.ui.theme.DeeplinkExampleTheme
-import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
 
@@ -46,8 +50,11 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
           ) {
-            Button(onClick = { deep.startActivity() }) {
-              Text(text = "Navigate To Deep Activity")
+            Column {
+              WaveView()
+              Button(onClick = { deep.startActivity() }) {
+                Text(text = "Navigate To Deep Activity")
+              }
             }
           }
         }
@@ -62,36 +69,57 @@ private fun WaveView() {
     modifier = Modifier
       .padding(8.dp)
       .clip(RoundedCornerShape(16.dp))
-      .background(Color(0xFF001769)),
+      .background(Color(0xFF21354f)),
   ) {
-    val width = constraints.maxWidth
-    val height = constraints.maxHeight
+    Box(
+      modifier = Modifier
+        .drawBehind {
+          drawPath(
+            path = Path().apply {
+              moveTo(0f, 0f)
+              lineTo(constraints.maxWidth.toFloat() / 4, 0f)
+              cubicTo(
+                constraints.maxWidth.toFloat() / 4,
+                0f,
+                200f,
+                constraints.maxHeight.toFloat(),
+                constraints.maxWidth.toFloat() + 100f,
+                constraints.maxHeight.toFloat()
+              )
+              lineTo(-constraints.maxWidth.toFloat(), constraints.maxHeight.toFloat())
+              close()
+            },
+            brush = Brush.horizontalGradient(
+              0.0f to Color(0xB221354F),
+              0.5f to Color(0xFF364960),
+              1.0f to Color(0xFF364960),
+            ),
+          )
+          drawPath(
+            path = Path().apply {
+              moveTo(0f, 0f)
+              lineTo(constraints.maxWidth.toFloat() / 8, 0f)
+              cubicTo(
+                constraints.maxWidth.toFloat() / 8,
+                0f,
+                100f,
+                constraints.maxHeight.toFloat(),
+                constraints.maxWidth.toFloat() / 2 + 50f,
+                constraints.maxHeight.toFloat()
+              )
+              lineTo(-constraints.maxWidth.toFloat(), constraints.maxHeight.toFloat())
+              close()
+            },
+            Brush.horizontalGradient(
+              0.0f to Color(0xB3364960),
+              0.5f to Color(0xFF2F425A),
+              1.0f to Color(0xFF2F425A),
+            ),
+          )
+        }
+    )
 
-    val mediumColoredPoint1 = Offset(0f, 0f)
-    val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-    val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-    val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-    val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
-
-    val mediumColorPath = Path().apply {
-      moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
-      standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
-      standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
-      standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
-      standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
-      lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-      lineTo(-100f, height.toFloat() + 100f)
-      close()
-    }
-
-    Canvas(modifier = Modifier) {
-      drawPath(
-        path = mediumColorPath,
-        color = Color(0xFF000000)
-      )
-    }
-
-    Box(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier.padding(16.dp)) {
       Text(
         text = LOREM,
         textAlign = TextAlign.Justify,
@@ -99,31 +127,29 @@ private fun WaveView() {
         fontFamily = FontFamily.Cursive,
         fontSize = 24.sp
       )
+
+      Spacer(modifier = Modifier.height(70.dp))
+
+      Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+          text = "http://example.com",
+          textAlign = TextAlign.Justify,
+          color = Color.White,
+          fontFamily = FontFamily.Cursive,
+          fontSize = 24.sp
+        )
+      }
     }
   }
-}
-
-private fun Path.standardQuadFromTo(from: Offset, to: Offset) {
-  quadraticBezierTo(
-    from.x,
-    from.y,
-    abs(from.x + to.x) / 2f,
-    abs(from.y + to.y) / 2f
-  )
 }
 
 private const val LOREM = "Quisque velit nisi, pretium ut lacinia in, elementum id enim. " +
     "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; " +
     "Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum " +
     "dolor sit amet, consectetur adipiscing elit. Donec rutrum congue leo eget malesuada. " +
-    "Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. " +
-    "Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur " +
-    "sed, convallis at tellus. Donec rutrum congue leo eget malesuada. Vestibulum ante ipsum " +
-    "primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, " +
-    "auctor sit amet aliquam vel, ullamcorper sit amet ligula. " +
-    "Donec rutrum congue leo eget malesuada."
+    "Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. "
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 500)
 @Composable
 private fun WaveViewPreview() {
   WaveView()
