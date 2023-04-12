7307c109fafecf37cb44da6a9bafaac9d62c62fe
Ghasem Shirdel <shirdelghasem79@gmail.com>
2023-04-12 14:59:32 +0330

    feat: add a bottom sheet

--- deep/src/main/java/com/example/deep/DeepActivity.kt
+++ deep/src/main/java/com/example/deep/DeepActivity.kt
@@ -3,15 +3,41 @@ package com.example.deep
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+import androidx.navigation.plusAssign
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 
 class DeepActivity : ComponentActivity() {
 
+  @OptIn(
+    ExperimentalAnimationApi::class,
+    ExperimentalMaterialNavigationApi::class
+  )
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     setContent {
-      DestinationsNavHost(navGraph = NavGraphs.root)
+      val navController = rememberAnimatedNavController()
+      val bottomSheetNavigator = rememberBottomSheetNavigator()
+      navController.navigatorProvider += bottomSheetNavigator
+
+      ModalBottomSheetLayout(
+        bottomSheetNavigator = bottomSheetNavigator,
+        sheetShape = RoundedCornerShape(16.dp),
+      ) {
+        DestinationsNavHost(
+          navGraph = NavGraphs.root,
+          navController = navController,
+          engine = rememberAnimatedNavHostEngine()
+        )
+      }
     }
   }
-
 }
--- deep/src/main/java/com/example/deep/Screens.kt
+++ deep/src/main/java/com/example/deep/Screens.kt
@@ -1,18 +1,22 @@
 package com.example.deep
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.unit.dp
 import com.example.deep.destinations.SecondScreensDestination
 import com.ramcosta.composedestinations.annotation.DeepLink
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.spec.DestinationStyle
 
 @RootNavGraph(start = true)
 @Composable
@@ -32,16 +36,20 @@ fun FirstScreen(navigator: DestinationsNavigator) {
 @Destination(
   deepLinks = [
     DeepLink(uriPattern = WEB)
-  ]
+  ],
+  style = DestinationStyle.BottomSheet::class
 )
 fun SecondScreens() {
-  Box(
-    modifier = Modifier.fillMaxSize(),
-    contentAlignment = Alignment.Center
-  ) {
-    val uriHandler = LocalUriHandler.current
-    Button(onClick = { uriHandler.openUri(WEB) }) {
-      Text("Go to the Web")
+  val uriHandler = LocalUriHandler.current
+
+  Column(modifier = Modifier.height(300.dp)) {
+    Box(
+      modifier = Modifier.fillMaxSize(),
+      contentAlignment = Alignment.Center
+    ) {
+      Button(onClick = { uriHandler.openUri(WEB) }) {
+        Text("Go to the Web")
+      }
     }
   }
 }
